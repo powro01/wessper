dc2c8fea6180f0b14541e24203193c2e09629184
powro01 <kamilpowroznik001@gmail.com>
2017-05-31 19:30:51 +0200

    next steps

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-new Siema();
+const slider = new Siema({
+    loop: true
+});
 
 var navigation = $('#navigation');
 var menuTrigger = $('#menuTrigger');
+var prevSlide = $('#prevSlide');
+var nextSlide = $('#nextSlide');
+var collapse = $('#collapse');
+var collapseList = $('.collapse');
 
 menuTrigger.on('click', function(e) {
     e.preventDefault();
     navigation.toggleClass('open');
 });
+
+prevSlide.on('click', function() {
+    slider.prev();
+});
+
+nextSlide.on('click', function() {
+    slider.next();
+});
+
+collapse.on('click', function(e) {
+    e.preventDefault();
+    collapseList.slideToggle();
+})
+
+setInterval(function() {
+    slider.next();
+}, 7000);
+
+$('a').on('click', function(e) {
+    e.preventDefault();
+})
